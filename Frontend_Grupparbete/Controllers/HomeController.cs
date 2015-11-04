19d507d72c3cdd424412ca3cb40d2a385fa11820
Stefan Bjerkell <stefanbjerkell@live.com>
2015-11-04 11:11:47 +0100

    Added Controllers: Home MixItUp

---
+++ Frontend_Grupparbete/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Frontend_Grupparbete.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+    }
+}
