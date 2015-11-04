ec4de4c82769da7747a19e086726e5623f993b12
Veckodag <Veckodag@gmail.com>
2015-11-04 13:09:22 +0100

    Basic functionallity of DataTables working

---
+++ Frontend_Grupparbete/Controllers/TheDataTableController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Frontend_Grupparbete.Controllers
+{
+    public class TheDataTableController : Controller
+    {
+        // GET: DataTable get.rekt();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
