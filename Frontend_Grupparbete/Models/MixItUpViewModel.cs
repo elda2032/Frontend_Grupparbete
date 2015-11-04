6360c3d60bcf5372097270c8f37434e6302c6f4c
Stefan Bjerkell <stefanbjerkell@live.com>
2015-11-04 16:24:04 +0100

    MixitUp and Layout

--- Frontend_Grupparbete/Models/MixItUpViewModel.cs
+++ Frontend_Grupparbete/Models/MixItUpViewModel.cs
@@ -1,33 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace Frontend_Grupparbete.Models
 {
-    using System.Web.WebSockets;
+    using System.Web.Mvc;
 
     public class MixItUpViewModel
     {
         public List<User> Users { get; set; }
 
-        public List<string> Professions { get; set; }
+        public List<SelectListItem> Professions { get; set; }
 
         public MixItUpViewModel(IEnumerable<User> users)
         {
             Users = new List<User>();
-            Professions = new List<string>();
-
+            Professions = new List<SelectListItem>();
+            Professions.Add(new SelectListItem() { Text = "Show All", Value = "All" });
             foreach (var user in users)
             {
                 Users.Add(user);
-                if (!Professions.Contains(user.Profession))
+                var found = false;
+                foreach (var item in this.Professions.Where(item => item.Value == user.Profession))
                 {
-                    Professions.Add(user.Profession);
+                    found = true;
                 }
 
+                if (!found) Professions.Add(new SelectListItem() { Text = user.Profession, Value = user.Profession.Split(' ')[0] });
             }
-        }
 
+        }
     }
 }
