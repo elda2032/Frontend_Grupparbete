558468e2bb0c898826bf51e04acfce963d9836bb
Veckodag <Veckodag@gmail.com>
2015-11-04 14:40:38 +0100

    Slow progress

--- Frontend_Grupparbete/Controllers/TheDataTableController.cs
+++ Frontend_Grupparbete/Controllers/TheDataTableController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using Frontend_Grupparbete.Models;
 
 namespace Frontend_Grupparbete.Controllers
 {
-    public class TheDataTableController : Controller
+    public class TheDataTableController : DatabaseController
     {
         // GET: DataTable get.rekt();
         public ActionResult Index()
         {
-            return View();
+            string userListJson = new JavaScriptSerializer().Serialize(Database.Users.ToList());
+            //System.IO.File.WriteAllText(@"C:\userlist.txt", userListJson);
+            return View(Database.Users);
         }
+        
     }
 }
