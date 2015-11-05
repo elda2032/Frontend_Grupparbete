08d900cc3c6314841b310588033f4bc345633c56
Elin <elda2032@gmail.com>
2015-11-05 12:19:30 +0100

    More weather.... Fixes for daily and added hourly

---
+++ Frontend_Grupparbete/Models/Weather/WeatherModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Frontend_Grupparbete.Models.Weather
+{
+    public abstract class WeatherModel
+    {
+        public string Summary { get; set; }
+        public string Icon { get; set; }
+        public DateTime Time { get; set; }
+
+        public decimal CloudCover { get; set; }
+        public decimal PrecipIntensity { get; set; }
+        public decimal PrecipProbability { get; set; }
+        
+        public decimal WindSpeed { get; set; }
+        public string WindBearingIcon { get; set; } 
+    }
+}
