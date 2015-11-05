86bf22fa42053bba0ad03fb87acbc5a0a723b109
Veckodag <Veckodag@gmail.com>
2015-11-05 10:58:09 +0100

    Small fun in the database!

--- Frontend_Grupparbete/Controllers/DatabaseController.cs
+++ Frontend_Grupparbete/Controllers/DatabaseController.cs
@@ -56,9 +56,26 @@ namespace Frontend_Grupparbete.Controllers
                         Password = "magic",
                         Profession = "Professional Haxx0r"
                     });
+            this.Database.Users.Add(AddUser("Dude@mailinator.com", "Henry", "Chuckle", 45000, "tragic", "Cheesecake Factory"));
+            this.Database.Users.Add(AddUser("PizzaGuy@mailinator.com", "Frank", "Pastrami", 9, "tragic", "Pizza Hut"));
+            this.Database.Users.Add(AddUser("Darth@vader.com", "Anakin", "Skywalker", 123456789, "sloth", "A Galaxy far, far away"));
             this.Database.SaveChanges();
         }
 
+        protected User AddUser(string email, string firstName, string lastName, int income, string password,
+            string profession)
+        {
+            User myUser = new User()
+            {
+                Email = email,
+                FirstName = firstName,
+                LastName = lastName,
+                Income = income,
+                Password = password,
+                Profession = profession
+            };
+            return myUser;
+        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
--- Frontend_Grupparbete/Controllers/TheDataTableController.cs
+++ Frontend_Grupparbete/Controllers/TheDataTableController.cs
@@ -13,14 +13,6 @@ namespace Frontend_Grupparbete.Controllers
         // GET: DataTable get.rekt();
         public ActionResult Index()
         {
-            // TODO: Format the list properly, in a similar way as data.txt
-            //var userList = Database.Users.ToList();
-
-
-            //string startFormat = @"{ ""data"": [ ";  
-            //var userListJson = new JavaScriptSerializer().Serialize(userList);
-            //var path = string.Format("{0}DataTableInsanity/userlist.txt", AppDomain.CurrentDomain.BaseDirectory);
-            //System.IO.File.WriteAllText(path, userListJson);
             return View(Database.Users);
         }
     }
