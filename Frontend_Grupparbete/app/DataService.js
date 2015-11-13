8f1050746f65d53e5b0d3ba21fc930fef40626f1
Stefan Bjerkell <stefanbjerkell@live.com>
2015-11-13 12:17:04 +0100

    Big Angular Register/Mange updates

---
+++ Frontend_Grupparbete/app/DataService.js
@@ -0,0 +1,14 @@
+﻿userFormApp.factory("dataService", ["$http", function ($http) {
+
+    var getUser = function (id) {
+        return $http.get("/Login/GetUser/" + id);
+    };
+    var addOrUpdateUser = function (user) {
+        return $http.post("/Login/UpdateUser", user);
+    }
+
+    return {
+        getUser: getUser,
+        addOrUpdateUser: addOrUpdateUser
+    };
+}])
---
+++ Frontend_Grupparbete/app/homeController.js
@@ -0,0 +1,21 @@
+﻿userFormApp.controller('homeController',
+    function homeController($scope, $modal, dataService) {
+
+        $scope.showCreateUserForm = function () {
+            $modal.open({
+                templateUrl: "/app/UserForm/userTemplate.html",
+                controller: "userController",
+                resolve: { id: 0 }
+            });
+        }
+
+        $scope.showUpdateUserForm = function (id) {
+            $modal.open({
+                templateUrl: "/app/UserForm/userTemplate.html",
+                controller: "userController",
+                resolve: { id: id }
+            });
+        }
+
+
+    });
